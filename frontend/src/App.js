e613f4045c9526fb6a6a52fc4b1c1a2f0880ab2f
“Jay <123779263+jxv210016@users.noreply.github.com>
2023-11-05 01:40:47 -0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,6 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-<<<<<<< Updated upstream
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [recentSearches, setRecentSearches] = useState([]);
@@ -106,7 +87,6 @@ const App = () => {
           {analysisResult}
         </div>
       </footer>
->>>>>>> Stashed changes
     </div>
   );
 }
